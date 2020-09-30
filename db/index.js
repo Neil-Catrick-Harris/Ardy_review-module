e29614caa132e938f997db21668a6ff55170fbcd
Matt Hong <matthew.m.hong@gmail.com>
2020-09-30 10:37:42 -0700

    removed package-lock.json and added to git ignote, set up environment vars to hide passwords and stuff

--- db/index.js
+++ db/index.js
@@ -1,15 +1,18 @@
 const Sequelize = require('sequelize');
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 mysql.createConnection({
-  user: 'root',
-  password: 'pineapple',
+  user: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
 }).then((db) => {
   db.query('CREATE DATABASE IF NOT EXISTS user_reviews').then(() => {
     db.end();
   });
 });
 
-const sequelize = new Sequelize('user_reviews', 'root', 'pineapple', { dialect: 'mysql', dialectOptions: { multipleStatements: true } }); // need to change for production to use env vars
+const sequelize = new Sequelize('user_reviews', `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, { dialect: 'mysql', dialectOptions: { multipleStatements: true } });
 
 module.exports = sequelize;
