80fe1c0d6ec8cea25393e899bf6210ba29100fa4
Matt Hong <matthew.m.hong@gmail.com>
2020-10-01 18:46:29 -0700

    removed commented out code

--- client/src/components/ReviewModal.jsx
+++ client/src/components/ReviewModal.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 import StarRatings from 'react-star-ratings';
 
 const reviewModal = ({ reviews }) => {
-  console.log('was here');
-  // 1-20 out of x
-
   const getReviewAverageForParam = (name) => {
     if (reviews.length === 0) {
       return 0;
@@ -15,20 +12,6 @@ const reviewModal = ({ reviews }) => {
     reviews.forEach((review) => { sum += review.score; });
     return sum / reviews.length;
   };
-
-  // product_id: i + 1,
-  // user: faker.internet.userName(),
-  // score: generateScore(),
-  // title: faker.lorem.words(),
-  // body: review,
-  // recommend: Math.random() >= 0.5,
-  // date: faker.date.past(),
-  // response_id: j === 0 ? i : null,
-  // ease: generateScore(),
-  // value: generateScore(),
-  // quality: generateScore(),
-  // appearance: generateScore(),
-
 
   return (
   <div>
