352b369d044e6efc7fa24ecdc13bdf146d40e9fe
ardydj <ardydj07@gmail.com>
2020-10-19 16:45:24 -0700

    Create Sequelize Instance With Postgres Dialect

--- db/dataGeneration.js
+++ db/dataGeneration.js
@@ -3,6 +3,8 @@ const faker = require('faker');
 
 let usernames = ['bruce445', 'adam1955', 'seeul8eraligator', 'purpledragon123', 'sk8tergurl89', 'donutluver96', 'gurlpow3rrox', 'stantheman301'];
 
+let titles = ['love ikea furniture', 'nice table but hard assembly', 'love hosting dinner parties with this dining set', 'needed a new lamp and this one works', 'arrived broken out of the box', 'the wood is chipping already', 'so for so good!', 'if you are looking for a comfy arm chair this is it!', 'makes our living room look good', 'Cheap quality do not buy!', 'awesome chair!'];
+
 let digits = [5, 3, 3, 5, 2, 1, 1, 2, 4, 2, 4];
 let digits2 = [2, 4, 1, 2, 5, 3, 4, 5, 1, 3, 4];
 
@@ -12,7 +14,7 @@ writer.write('product_id,user,score,title,body,recommend,date,ease,value,quality
 
 const generateCSVData = function (writer, encoding, callback) {
 
-  let recordCount = 100000;
+  let recordCount = 10000000;
   let product_id = 1;
   writeFunction();
   function writeFunction () {
@@ -27,7 +29,7 @@ const generateCSVData = function (writer, encoding, callback) {
         for (let i = 0; i < reviewCount; i++) {
           let user = usernames[Math.floor(Math.random() * 8)];
           let score = digits[i];
-          let title = faker.random.words();
+          let title = titles[i];
           let body = 'body for review regarding reviews about mykea product and how I need nice furniture and for my new apartment that is needed to make it look nice';
           let recommend = digits2[i];
           let date = faker.date.recent();
--- db/postgreSQL/postgresDB.js
+++ db/postgreSQL/postgresDB.js
@@ -1,2 +1,17 @@
 const Sequelize = require('sequelize');
-debugger;
+const sequelize = new Sequelize('mykea-reviews', 'ncg8', 'password', {
+  dialect: 'postgres',
+  host: 'localhost',
+  logging: false
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('connection has been established successfully');
+  })
+  .catch(err => {
+    console.error('unable to connect to the database: ', err);
+  });
+
+// const Review = sequelize.
