a85360eb148323e8c34981e5168cf00b73d0bbd9
ardydj <ardydj07@gmail.com>
2020-10-20 14:04:49 -0700

    Resolved Postgres Seeding Script Error

---
+++ db/postgreSQL/postgresSchema.sql
@@ -0,0 +1,22 @@
+
+DROP DATABASE IF EXISTS reviews_module;
+
+CREATE DATABASE reviews_module;
+
+\c reviews_module;
+
+CREATE TABLE IF NOT EXISTS reviews (
+  id SERIAL PRIMARY KEY,
+  product_id INT,
+  _user VARCHAR(100),
+  score INT,
+  title VARCHAR(255),
+  body VARCHAR(600),
+  recommend INT,
+  review_date VARCHAR(100),
+  ease INT,
+  product_value INT,
+  quality INT,
+  appearance INT,
+  works INT
+);
